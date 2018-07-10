12c0c72921c3647ca93915980513a4f9de0f0eae
Taras Chornyi <taras.chornyi.v@gmail.com>
2018-07-10 01:10:43 -0500

    root

---
+++ test_site/js/main.js
@@ -0,0 +1,8 @@
+function mobilMenu() {
+    var x = document.getElementById("mobilMenuLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
